f446e29e258064174e5400401398141365a3ed7c
Where.LIU <liuzhaowei55@gmail.com>
2021-03-31 13:44:41 +0800

    feat: tailwindcss

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -1,8 +1,10 @@
+<!doctype html>
 <html>
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>UCenter</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body></body>
 </html>
