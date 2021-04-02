1907d5f2bcdd8f3cecc5d93dd10df91eaa142cb2
Where.LIU <liuzhaowei55@gmail.com>
2021-04-02 11:49:05 +0800

    feat: save

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', [LoginController::class, 'loginForm'])->name('login');
+Route::get('/login', [LoginController::class, 'loginForm'])->name('auth.login');
 Route::post('/login', [LoginController::class, 'login']);
-Route::get('/register', [RegisterController::class, 'registerForm'])->name('register');
+Route::get('/register', [RegisterController::class, 'registerForm'])->name('auth.register');
 Route::post('/register', [RegisterController::class, 'register']);
 
 Route::middleware('web')->group(function () {
